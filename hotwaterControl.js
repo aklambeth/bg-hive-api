4a778ad2165d33335e667703335e4683a46b33e3
adrian <a@b.com>
2015-01-19 23:19:14 +0000

    Created the hot water controller

---
+++ hotwaterControl.js
@@ -0,0 +1,8 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+var request = require('request');
+var async = require("async");
+
+
+module.exports = HotWaterControl;
+
